b118440bfbba2645eda8999235deb504def6a1a0
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2024-09-24 16:20:02 +0000

    Remover "/#/" da url

--- todo-list-web/src/router/index.ts
+++ todo-list-web/src/router/index.ts
@@ -1,8 +1,6 @@
 import { route } from 'quasar/wrappers';
 import {
-  createMemoryHistory,
   createRouter,
-  createWebHashHistory,
   createWebHistory,
 } from 'vue-router';
 
@@ -18,9 +16,7 @@ import routes from './routes';
  */
 
 export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+  const createHistory = createWebHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
